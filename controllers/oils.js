fecf1b7be9b0468a9e2521d7037a55759c4f0526
Sarah Anderson <sarahanderson@MacBook-Pro.local>
2017-08-05 11:08:24 -0600

    add oil functionality working

--- controllers/oils.js
+++ controllers/oils.js
@@ -4,7 +4,11 @@ const Oil = require('../models/oils.js');
 const router = express.Router();
 
 router.get('/', (req, res) =>{
-  res.render('oils/index.ejs');
+  Oil.find({}, (err, foundOils) => {
+    res.render('oils/index.ejs', {
+      oils: foundOils
+    });
+  })
 });
 
 router.post('/', (req, res) => {
@@ -15,6 +19,14 @@ router.post('/', (req, res) => {
 
 router.get('/new', (req, res) => {
   res.render('oils/new.ejs');
+});
+
+router.get('/:id', (req, res) => {
+  Oil.findById(req.params.id, (err, foundOil) => {
+    res.render('oils/show.ejs', {
+      oil: foundOil
+    });
+  });
 });
 
 module.exports = router;
