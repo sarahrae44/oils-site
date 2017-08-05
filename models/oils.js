7f3054d6e957bddc6eee427961ab97af2afaa771
Sarah Anderson <sarahanderson@MacBook-Pro.local>
2017-08-05 11:25:49 -0600

    oils show page working

--- models/oils.js
+++ models/oils.js
@@ -2,12 +2,16 @@ const mongoose = require('mongoose');
 // const Body = require('./bodies.js');
 
 const oilSchema = mongoose.Schema({
-  name: String,
-  bgImg: String,
-  iconImg: String,
-  bodySystemName1: String,
-  bodySystemText1: String
+  name: { type: String, required: true },
+  bgImg: { type: String, required: true },
+  iconImg: { type: String, required: true },
+  bodySystemName1: { type: String, required: true },
+  bodySystemText1: { type: String, required: true },
   // optional below
+  bodySystemName2: String,
+  bodySystemText2: String,
+  bodySystemName3: String,
+  bodySystemText3: String
 });
 
 const Oil = mongoose.model('Oil', oilSchema);
