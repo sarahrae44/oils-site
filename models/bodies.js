7aaaa6c7b24f3e187f4144bd544d14d9fcdad777
Sarah Anderson <sarahanderson@MacBook-Pro.local>
2017-08-08 12:07:08 -0600

    cleaned up some of the data

--- models/bodies.js
+++ models/bodies.js
@@ -4,18 +4,9 @@ const Oil = require('./oils.js');
 const bodySchema = mongoose.Schema({
   name: String,
   desc: String,
-  bgImg: String,
   iconImg: String,
   oilEffectName: String,
-  oilEffectText: String
-  // optional below
-  // oilEffectName2: String,
-  // oilEffectText2: String,
-  // oilEffectName3: String,
-  // oilEffectText3: String,
-  // oilEffectName4: String,
-  // oilEffectText4: String,
-  // oils: [Oil.schema]
+  oilEffectText: String,
 });
 
 const Body = mongoose.model('Body', bodySchema);
--- models/oils.js
+++ models/oils.js
@@ -4,17 +4,9 @@ const Body = require('./bodies.js');
 const oilSchema = mongoose.Schema({
   name: String,
   desc: String,
-  bgImg: String,
   iconImg: String,
   bodySystemName: String,
   bodySystemText: String,
-  // optional below
-  // bodySystemName2: String,
-  // bodySystemText2: String,
-  // bodySystemName3: String,
-  // bodySystemText3: String,
-  // bodySystemName4: String,
-  // bodySystemText4: String,
   bodies: [Body.schema]
 });
 
